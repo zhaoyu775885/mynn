e3a5ab1add1480f2685092a85f0633fe8e04fdf2
Yu Zhao <zhaoyu775885@gmail.com>
2018-11-08 01:16:56 +0800

    init my own NN

---
+++ GDTF.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 31 11:40:52 2018
+
+@author: ZhaoYu
+"""
+
+import tensorflow as tf
+
+if __name__ == '__main__':
+    print('tensorflow implemented LR')
+    
+    tf.Variables
+    tf.placeholder
