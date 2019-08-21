5701b165b9176bda9758c2407fc1a41f3d5b5a81
zhaoyu775885 <zhaoyu775885@163.com>
2019-08-22 01:13:20 +0800

    prepare pytorch resnet & all other interesting nets

---
+++ pytorch/models/resnet.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ResidualBlock(nn.Module):
+    def __init__(self):
+        pass
+
+
+class Bottleneck(nn.Module):
+    def __init__(self):
+        super(self, )
+        pass
+
+class ResNet(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
