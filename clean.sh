78a65443609bea500799c56dd6c5b7b141ce0e87
zhaoyu775885 <zhaoyu775885@163.com>
2020-01-28 22:04:54 +0800

    revise clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
 
-rm -f *~ .*~
+clean_recursive() {
+    echo ${1}
+    rm -rf ${1}/*~ ${1}/.*~ ${1}/__pycache__
+    for file in ${1}/*
+    do
+        if [ -d ${file} ]
+        then
+            clean_recursive ${file}
+        fi
+    done
+}
+
+clean_recursive ./
