26255d546b87a9c6ae7c4dcffb553c427db98750
zhaoyu775885 <zhaoyu775885@163.com>
2020-03-01 18:01:07 +0800

    update dataset split and test

--- pytorch/NameClassify/network.py
+++ pytorch/NameClassify/network.py
@@ -23,8 +23,8 @@ class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
-        self.rnn = nn.RNN(input_size, self.hidden_size, num_layers=2, 
-                batch_first=True, bidirectional=True, nonlinearity='tanh')
+        self.rnn = nn.GRU(input_size, self.hidden_size, num_layers=2,
+                batch_first=True, bidirectional=True)
         self.h2o = nn.Linear(self.hidden_size*2, output_size)
 
     def forward(self, inputs):
