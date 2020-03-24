b70c051361c85a1812eefbe8ff6f95cbbc0f9eab
zhaoyu775885 <zhaoyu775885@163.com>
2020-03-24 23:30:35 +0800

    build up primary modules for Star-Transformer

---
+++ pytorch/StarTransformer/tasks.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Task 1: Text Classification
+Task 2: Natural Language Inference
+Task 3: Sequence Labelling 
+
+All 3 tasks implement Transformer and Star-Transformer as encoder
+"""
