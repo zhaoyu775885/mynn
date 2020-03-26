48da7392ef349b21f8c4053822aab0c930657346
zhaoyu775885 <zhaoyu775885@163.com>
2020-03-26 20:11:06 +0800

    Update learner.py

--- pytorch/TextClassify/learner.py
+++ pytorch/TextClassify/learner.py
@@ -17,22 +17,24 @@ class Learner():
         # set device & build dataset
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         self.data_path = dataset_path
-        self.train_path = os.path.join(self.data_path, 'train.tsv')
-        self.test_path = os.path.join(self.data_path, 'test.tsv')
-        
+
         print('begin training data')
-        self.dataset_train = Dataset(self.train_path)
-#        print('begin test data')
-#        self.dataset_test = Dataset(self.test_path)
+        self.dataset = Dataset(self.data_path)
+        #self.dataset_train = Dataset(self.train_path)
+        #self.dataset_test = Dataset(self.test_path)
         print('finished data building')
         
         # build dataloader
         self.trainloader = self._build_dataloader(BATCH_SIZE, train=True)
-#        self.testloader = self._build_dataloader(1, train=False)
+        self.testloader = self._build_dataloader(1, train=False)
 
-        vocab_size = self.dataset_train.vocab_size
-        ninp, nhid, nclass = 300, 512, 5
-        self.net = network(vocab_size, ninp, nhid, nclass).to(self.device)
+        vocab_size = self.dataset.vocab_size
+        ninp, nhid, nclass = 300, 512, 2
+        # for CNN, RNN and CRNN
+        #self.net = network(vocab_size, ninp, nhid, nclass).to(self.device)
+        # for Transformer and Star-Transformer
+        nhead, nlayer = 4, 1
+        self.net = network(self.dataset.vocab, ninp, nhead, nhid, nlayer, nclass).to(self.device)        
 
         # setup loss function and optimizer
         self.criterion = self._loss_fn()
@@ -40,9 +42,7 @@ class Learner():
         self.lr_scheduler = self._setup_lr_scheduler()
 
     def _build_dataloader(self, batch_size, train=True):
-        if train:
-            return self.dataset_train.build_iterator(batch_size, train)
-        return self.dataset_test.build_iterator(batch_size, train)
+        return self.dataset.build_iterator(batch_size, train)
     
     def _loss_fn(self):
         return nn.CrossEntropyLoss()
@@ -62,40 +62,52 @@ class Learner():
     
     def train(self, n_epoch=25):
         print('Begin training:')
+        pred_correct = 0
+        total_cnt = 0
+        total_loss = 0
         for epoch in range(n_epoch):
             self.net.train()
             for i, batch in enumerate(self.trainloader):
-                batch_size = batch.Phrase.shape[-1]
+                batch_size = batch.text.shape[-1]
                 hiddens = self.net.init_hiddens(batch_size).to(self.device)
-                print(i, batch.Sentiment, self.dataset_train.raw(batch.Phrase))
-                texts, labels = batch.Phrase.to(self.device), batch.Sentiment.to(self.device)
+                #print(i, batch.label, self.dataset_train.raw(batch.text))
+                texts, labels = batch.text.to(self.device), batch.label.to(self.device)
+                #print(texts.shape, labels.shape)
                 self.opt.zero_grad()
                 logits = self.net(texts, hiddens)
+                #print(logits.shape, labels.shape)
                 accuracy, loss = self.metrics(logits, labels)
                 loss.backward()
                 self.opt.step()
+                pred_correct += accuracy * batch_size
+                total_cnt += batch_size
+                total_loss += batch_size*loss
                 if (i+1) % 100 == 0:
-                    print(batch.Phrase.shape)
+                    #print(batch.text.shape)
                     print(i+1, ' acc={0:.2f}, loss={1:.3f}'.format(accuracy*100, loss))
+            self.test()
             print(epoch+1, 'finished')
             self.lr_scheduler.step()
-#            self.test()
-#        print('Finished Training')
-#
-#    def test(self):
-#        pred_correct = 0
-#        self.dataset_test.init_batch_loader()
-#        n_iters = self.dataset_test.n_iters()
-#        print(n_iters, 'iterations')
-#        for i, data in enumerate(self.testloader):
-#           feats, labels, lengths = data
-#           local_max_length = max(lengths)
-#           inputs = feats.to(self.device)
-#           outputs = self.net(inputs[:, :local_max_length, :])
-#           accuracy, loss = self.metrics(outputs[:,-1,:], labels.to(self.device))
-#           pred_correct += accuracy
-#           # if (i+1) % 1000 == 0:
-#           #     print(i+1, accuracy)
-#           if i == n_iters - 1:
-#               print('test acc={0:.2f}'.format(pred_correct * 100 / n_iters))
-#               break
+        print('Finished Training')
+
+    def test(self):
+        pred_correct = 0
+        total_cnt = 0
+        total_loss = 0
+        self.net.eval()
+        with torch.no_grad():
+            for i, batch in enumerate(self.testloader):
+                batch_size = batch.text.shape[-1]
+                hiddens = self.net.init_hiddens(batch_size).to(self.device)
+                texts, labels = batch.text.to(self.device), batch.label.to(self.device)
+                logits = self.net(texts, hiddens)
+                accuracy, loss = self.metrics(logits, labels)
+                pred_correct += accuracy*batch_size
+                total_cnt += batch_size
+                total_loss += loss
+                '''
+                if (i+1) % 1000 == 0:
+                    print('{0}: test acc={1:.2f}, pred_cor={2}, total={3}'.format(i+1,
+                        pred_correct * 100 / total, pred_correct, total))
+                '''
+            print('test acc={0:.2f}, loss={1:.2f}'.format(pred_correct * 100 / total_cnt, total_loss/total_cnt))
