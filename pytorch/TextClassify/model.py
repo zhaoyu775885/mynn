5c1c894c08249b5f1c455182bcb5b1fb4629fc26
zhaoyu775885 <zhaoyu775885@163.com>
2020-03-26 20:16:00 +0800

    Update model.py

--- pytorch/TextClassify/model.py
+++ pytorch/TextClassify/model.py
@@ -15,22 +15,34 @@ import torch.nn.functional as F
 import math
 
 class RNN(nn.Module):
-    def __init__(self, vocab_size, nemb, nhid, nclass, nlayer=2):
+    '''
+    hyperparameters:
+        batch_size=32, init_lr=2.5e-2, weight_decay=1e-5, lr_decay=0.1: 83.50%
+    '''
+    def __init__(self, vocab_size, nemb, nhid, nclass, nlayer=1, dropout=0.2):
         super(RNN, self).__init__()
         self.nemb = nemb
         self.nhid = nhid
         self.nclass = nclass
         self.nlayer = nlayer
         self.emb = nn.Embedding(num_embeddings=vocab_size, embedding_dim=nemb)
-        self.rnn = nn.GRU(input_size=nemb, hidden_size=nhid, num_layers=nlayer)
-        self.fc = nn.Linear(nhid, nclass)
+        self.dropout1 = nn.Dropout(dropout)
+        self.rnn = nn.GRU(input_size=nemb, hidden_size=nhid, num_layers=nlayer, 
+                dropout=0 if nlayer==1 else dropout, bidirectional=True)
+        self.num_dir = 2
+        self.dropout2 = nn.Dropout(dropout)
+        self.fc = nn.Linear(nhid*self.num_dir, nclass)
         
     def forward(self, x, hidden_state):
         emb = self.emb(x)
+        emb = self.dropout1(emb)
         rnn_output, hidden_state = self.rnn(emb, hidden_state)
-        logits = self.fc(rnn_output)
-        return logits[-1,...]
+        output = self.dropout2(rnn_output[-1,...])
+        logits = self.fc(output)
+        return logits
     
     def init_hiddens(self, batch_size):
-        return torch.zeros([self.nlayer, batch_size, self.nhid])
+        return torch.zeros([self.nlayer*self.num_dir, batch_size, self.nhid])
+    
+   
         
