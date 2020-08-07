1e928005102265e1f9d83addb307cf8194eb6ed5
zhaoyu <zhaoyu775885@163.com>
2020-08-07 17:17:55 +0800

    0807

--- DCPS/main.py
+++ DCPS/main.py
@@ -2,6 +2,7 @@ from datasets.cifar import Cifar100
 from nets.resnet import ResNet20, ResNet32
 from nets.resnet_lite import ResNet20Lite
 from learner.learner import Learner
+from learner.learner_dcps import DLearner
 
 if __name__ == '__main__':
     # specify dataset
@@ -10,12 +11,12 @@ if __name__ == '__main__':
     dataset = Cifar100(cifar100)
 
     # specify network model
-    net = ResNet20(n_classes=100)
-    # net = ResNet20Lite(n_classes=100)
+    # net = ResNet20(n_classes=100)
+    net = ResNet20Lite(n_classes=100)
     # net = Lenet()
 
     # init Leaner
-    learner = Learner(dataset, net)
+    learner = DLearner(dataset, net)
 
     learner.train(n_epoch=200)
     learner.load_model()
